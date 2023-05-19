1187b48a31ad3261a2fc045dca13aee814a104c0
Marcelo <mbellotto@gmail.com>
2023-05-18 23:54:21 -0300

    Update 001
    First version with JSON reader (WIP)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use serde::{Deserialize};
+
+#[derive(Debug, Deserialize)]
+struct Person {
+    name: String,
+    age: u32,
+    city: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Read the JSON file
+    let json_str = std::fs::read_to_string("./token.definition.json")
+        .expect("Failed to read the file.");
+
+    // Deserialize the JSON into a Person struct
+    let person: Person = serde_json::from_str(&json_str)
+        .expect("Failed to deserialize JSON.");
+
+    // Print the deserialized object
+    println!("{:?}", person);
 }
